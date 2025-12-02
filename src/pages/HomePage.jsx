0118c2176213814763d5df1b4171552fa76b91e8
Liang <liang.huang@marinethinking.com>
2025-12-02 15:26:43 -0400

    video

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
+import VideoDemo from '../components/VideoDemo'
 import BetaDisclaimer from '../components/BetaDisclaimer'
 import Footer from '../components/Footer'
 
@@ -10,6 +11,7 @@ function HomePage() {
             <Navigation />
             <Hero />
             <Features />
+            <VideoDemo />
             <BetaDisclaimer />
             <Footer />
         </>
